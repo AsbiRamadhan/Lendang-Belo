cdd5d62226b7034a7d22566e99207f17bba3468f
AsbiRamadhan <asbiramadhan24@gmail.com>
2026-08-10 14:15:38 +0700

    kedua

---
+++ app.js
@@ -0,0 +1,21 @@
+const next = require("next");
+
+const app = next({
+  dev: false,
+  hostname: "0.0.0.0",
+  port: process.env.PORT || 3000,
+});
+
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const http = require("http");
+
+  http
+    .createServer((req, res) => {
+      handle(req, res);
+    })
+    .listen(process.env.PORT || 3000, "0.0.0.0", () => {
+      console.log("Next.js server started");
+    });
+});
